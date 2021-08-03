7c366277e09c451cc148f6e9ad4f3c90916c33ed
Alfir Tyakin <alfir777@rambler.ru>
2021-08-03 18:32:38 +0500

    add json_reformat

---
+++ scripts/json_reformat.py
@@ -0,0 +1,20 @@
+import ast
+import json
+import os
+
+
+def json_reformat(input_file, indent=4, sort_keys=True):
+    if os.path.exists(input_file):
+        with open(input_file, 'r', encoding='utf8') as file:
+            for line in file:
+                data = line
+        data = ast.literal_eval(data)
+        json_data = json.dumps(data, indent=indent, sort_keys=sort_keys)
+        with open('reformat_file.json', 'w', encoding='utf8') as file:
+            file.write(json_data)
+    else:
+        print('File not found')
+
+
+file_json = '../1.json'
+json_reformat(file_json, indent=4, sort_keys=False)
