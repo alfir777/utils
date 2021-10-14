7fe65e40d11fbd187200a62f48a615e5793e1de0
Alfir Tyakin <alfir777@rambler.ru>
2021-10-14 11:50:46 +0500

    update conversion_string

--- example/conversion_string.py
+++ example/conversion_string.py
@@ -5,22 +5,41 @@
 # ..
 # test-100-asdasda.dasda.sdsda
 
+# часть 2
+# tmp = 'aa-bb-ccc-ddddddd-[1:5].ee.xx.ff'
+# tmp = 'aa-bb-[1-15]-ccc-ddddddd-[1:5].ee.xx.ff'
+#
+# часть 3
+# aa-bb-cc.dd - это url
+# поступает строка aa-[1-3]-bb.cc-dd-[1-2].gg.ru tcp 80, 85-90
+# делаем из нее список
+#
+# aa-1-bb.cc-dd-1.gg.ru tcp 80
+# aa-1-bb.cc-dd-1.gg.ru tcp 85-90
 
-def output_string(line, index_list, numbers_list):
 
+def output_string(line, index_list, numbers_list, ports_list):
+    line = line.split()
+    url = line[0]
+    protocol = line[1]
     if len(numbers_list) == 1:
-        for i in range(numbers_list[0][0], numbers_list[0][1] + 1):
-            print(f'{line[:index_list[0][0]]}-{i}-{line[index_list[0][1]+2:]}')
+        for ports in ports_list:
+            for i in range(numbers_list[0][0], numbers_list[0][1] + 1):
+                print(f'{url[:index_list[0][0]]}-{i}-{url[index_list[0][1] + 2:]} {protocol} {ports}')
     elif len(numbers_list) == 2:
-        for i in range(numbers_list[0][0], numbers_list[0][1]):
-            for y in range(numbers_list[1][0], numbers_list[1][1]):
-                print(f'{line[:index_list[0][0]]}'
-                      f'{i}'
-                      f'{line[index_list[0][1]+1:index_list[1][0]]}'
-                      f'{y}'
-                      f'{line[index_list[1][1]+1:]}')
+        for ports in ports_list:
+            for i in range(numbers_list[0][0], numbers_list[0][1]+1):
+                for y in range(numbers_list[1][0], numbers_list[1][1]+1):
+                    print(f'{url[:index_list[0][0]]}'
+                          f'{i}'
+                          f'{url[index_list[0][1] + 1:index_list[1][0]]}'
+                          f'{y}'
+                          f'{url[index_list[1][1] + 1:]}'
+                          f' {protocol}'
+                          f' {ports}'
+                          )
     else:
-        print('Не предусмотрено более двух')
+        print('Не предусмотренная строка')
 
 
 def conversion_string(line):
@@ -33,6 +52,16 @@ def conversion_string(line):
             index_close.append(i)
     index_list = list(zip(index_open, index_close))
 
+    line_list = line.split()
+    ports_list = []
+    for i, value in enumerate(line_list):
+        if value in PROTOCOL_LIST:
+            ports_list = line_list[i + 1:]
+            break
+    for i, value in enumerate(ports_list):
+        if value.find(', '):
+            ports_list[i] = value.replace(',', '')
+
     numbers_list = []
     for i in index_list:
         numbers = line[i[0]:i[1]]
@@ -41,12 +70,15 @@ def conversion_string(line):
                 numbers = numbers.replace(numbers[y], ' ')
         numbers_list.append(list(map(int, numbers.split())))
 
-    output_string(line, index_list, numbers_list)
+    output_string(line, index_list, numbers_list, ports_list)
 
+
+PROTOCOL_LIST = ['tcp', 'udp']
 
 # tmp = input()
 # tmp = 'test-[1-100]-asdasda.dasda.sdsda'
 # tmp = 'aa-bb-ccc-ddddddd-[1:5].ee.xx.ff'
-tmp = 'aa-bb-[1-15]-ccc-ddddddd-[1:5].ee.xx.ff'
+# tmp = 'aa-bb-[1-15]-ccc-ddddddd-[1:5].ee.xx.ff'
+tmp = 'aa-[1-4]-bb.cc-dd-[1-2].gg.ru tcp 80, 85-90'
 
 conversion_string(tmp)
